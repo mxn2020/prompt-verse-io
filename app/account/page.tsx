9d92fdac667163e75a66b49ec1fa597cdfd7f8eb
Mehdi Nabhani <mehdi@nabhani.de>
2025-05-31 16:03:10 +0200

    test

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,11 +2,18 @@
 import { AccountPageClient } from '@/components/account/account-page-client';
 import { requireAuth, getUserProfile } from '@/lib/auth';
 
-export const dynamic = "force-dynamic";
-
 export default async function AccountPage() {
   const user = await requireAuth();
+
+  if (!user) {
+    // If user is not authenticated, redirect to login
+    return null; // This will be handled by the requireAuth function
+  }
+
   const profile = await getUserProfile(user.id);
 
-  return <AccountPageClient user={user} initialProfile={profile} />;
+  return <AccountPageClient
+    user={user}
+    initialProfile={profile}
+  />;
 }
